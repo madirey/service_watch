6255f71bd7626aba2c2319a845e668b6122590e0
Michael Fine <mfine15@gmail.com>
2012-06-16 17:02:19 -0400

    added sower template

--- servicewatch/urls.py
+++ servicewatch/urls.py
@@ -22,6 +22,7 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+    (r'^comments/', include('django.contrib.comments.urls')),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^api/', include(v1_api.urls)),

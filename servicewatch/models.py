5baaa943a77b7a10bba14e6d1278b2dd9f59d070
Michael Fine <mfine15@gmail.com>
2012-06-16 10:35:13 -0400

    added Sower and Grower

--- servicewatch/models.py
+++ servicewatch/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Sower(models.model):
+	user = models.ForeignKey(User)
+	description = models.CharField(limit=1000)
+	rating = RatingField(range=5)
+class Grower(models.model):
+	user = modles.ForeignKey(User)
+	rating = RatingField(range=5)
+class Task(models.model):
 
-class Sower(models.model)

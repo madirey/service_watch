f4639a9f711be13ec527fd39c44041e5ef6f955b
Matt Caldwell <matt.caldwell@gmail.com>
2012-06-16 21:42:20 -0400

    home/landing page

--- servicewatch/views.py
+++ servicewatch/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import redirect, render_to_response
 from django.template import RequestContext
 from servicewatch.models import Sower 
 
+def home(request):
+	return render_to_response('Home.html', { },
+			context_instance=RequestContext(request))
+
 def sower_profile(request, id):
 	return render_to_response('Sower.html', { 'sower': Sower.objects.get(id=id) },
 			context_instance=RequestContext(request))

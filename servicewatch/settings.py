2da375b54d3e23a5338184d1936f1ee00732aa97
Matt Caldwell <matt.caldwell@gmail.com>
2012-06-17 13:33:14 -0400

    postgres username/password

--- servicewatch/settings.py
+++ servicewatch/settings.py
@@ -13,8 +13,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'servicewatch',                             # Or path to database file if using sqlite3.
-        'USER': 'admin',                                 # Not used with sqlite3.
-        'PASSWORD': 'admin',                             # Not used with sqlite3.
+        'USER': 'postgres',                                 # Not used with sqlite3.
+        'PASSWORD': 'abcd1234',                             # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
